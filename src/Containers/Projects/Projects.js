3adfbf9aff2fcaf0c09030e4b83d439ba7e7b9aa
Ferdy <ferdysantoso@icloud.com>
2020-01-27 22:37:17 +0700

    Add my projects to 'projects' page

--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
 import './Projects.css'
+import Card from 'react-bootstrap/Card'
 
 class Projects extends Component {
     render() {
         return (
             <div className="projects-container">
                 <h1 className="projects-title">Projects</h1>
+                <div className="projects-flex">
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>ITB NIM Finder</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">HTML | CSS | JS | REACT</Card.Subtitle>
+                            <Card.Text>
+                                A NIM Finder project for ITB. Made for
+                                Programming Laboratory selection
+                                assignment.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>Congklak Minimax</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Python</Card.Subtitle>
+                            <Card.Text>
+                                A Congklak Game Bot, made using
+                                Artificial Intelligence : Minimax
+                                Algorithm and Alpha-Beta Pruning.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>Engima Cinema Website</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">HTML | CSS | JS | PHP</Card.Subtitle>
+                            <Card.Text>
+                                A Cinema Website called Engima,
+                                simulating a real cinema website.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>TCP UDP Socket Connection</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Python</Card.Subtitle>
+                            <Card.Text>
+                                A UDP Connection simulating a TCP
+                                connection, used for transferring files.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>BFS and A* Maze Solver</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Python</Card.Subtitle>
+                            <Card.Text>
+                                A Maze Solver program using BFS and
+                                A* Algorithm.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }} bg="dark" border="secondary" className="projects-card">
+                        <Card.Body>
+                            <Card.Title>Object Oriented Engi's Farm</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">C++</Card.Subtitle>
+                            <Card.Text>
+                                A harvest-moon-like game developed
+                                using object oriented programming
+                                paradigm.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
             </div>
         )
     }
