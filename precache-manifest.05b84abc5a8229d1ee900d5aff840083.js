2c4dd8eefa211afe95f0a8f72780c6a7cd0b2159
Ferdy <ferdysantoso@icloud.com>
2020-01-30 11:45:57 +0700

    Updates

---
+++ precache-manifest.05b84abc5a8229d1ee900d5aff840083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44244ca6eaae48ef2d9b606aa8dd4f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d1197e795b83e753f9",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "511ad73413a0acde05fc",
+    "url": "/static/css/main.b97d446d.chunk.css"
+  },
+  {
+    "revision": "23d1197e795b83e753f9",
+    "url": "/static/js/2.15aa283a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.15aa283a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "511ad73413a0acde05fc",
+    "url": "/static/js/main.8939c0ce.chunk.js"
+  },
+  {
+    "revision": "71a930cf8f5843bc37f6",
+    "url": "/static/js/runtime-main.f93b8ee2.js"
+  }
+]);
