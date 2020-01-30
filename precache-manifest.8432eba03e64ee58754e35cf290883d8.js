2ed8225ec32ccbd8da7d7beca9bab3ec7ca16ae0
Ferdy <ferdysantoso@icloud.com>
2020-01-30 09:34:21 +0700

    Updates

---
+++ precache-manifest.8432eba03e64ee58754e35cf290883d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4228867ec37fe2abfb4f44a67293aeaf",
+    "url": "/ferdy-website/index.html"
+  },
+  {
+    "revision": "23d1197e795b83e753f9",
+    "url": "/ferdy-website/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3db5e056710769f99e3b",
+    "url": "/ferdy-website/static/css/main.b97d446d.chunk.css"
+  },
+  {
+    "revision": "23d1197e795b83e753f9",
+    "url": "/ferdy-website/static/js/2.15aa283a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ferdy-website/static/js/2.15aa283a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3db5e056710769f99e3b",
+    "url": "/ferdy-website/static/js/main.1bcf5f51.chunk.js"
+  },
+  {
+    "revision": "d0e221e618f7081cfdd3",
+    "url": "/ferdy-website/static/js/runtime-main.f89758ad.js"
+  }
+]);
