8263a6d123c4e5e3eae367bb64660365e44229cb
ferdysan360 <ferdysantoso@icloud.com>
2021-11-19 11:04:42 +0700

    Update About page and Fix company logos

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './About.css'
-import '../../Assets/facebook.png'
+import FacebookLogo from '../../Assets/facebook.png'
+import LinkedInLogo from '../../Assets/linkedin.png'
+import GithubLogo from '../../Assets/github.png'
+import GitlabLogo from '../../Assets/gitlab.png'
+import GmailLogo from '../../Assets/gmail.png'
 
 class About extends Component {
     render() {
@@ -10,7 +14,7 @@ class About extends Component {
                 <div className="about-flex">
                     <div className="about-item-me">
                         <p className="me-paragraph">
-                            Hi! My name is Ferdy Santoso and I am a Frontend Web Developer. I studied 
+                            Hi! My name is Ferdy Santoso and I am a Software Engineer. I studied 
                             in the field of Computer Science/Informatics at Institut Teknologi Bandung,
                             Indonesia. I am currently living in Semarang. I am passionate in software 
                             enginnering and I like developing software.
@@ -20,7 +24,7 @@ class About extends Component {
                         {/* <h4 className="contacts-header">Find me on...</h4>
                         <div className="contacts-item">
                             <a href="https://www.facebook.com/ferdy.santoso.520" target="_blank" rel="noopener noreferrer">
-                                <img src={require("../../Assets/facebook.png")} alt="facebook"/>
+                                <img src={FacebookLogo} alt="facebook"/>
                                 <h5>Ferdy Santoso</h5>
 
                             </a>
@@ -28,28 +32,28 @@ class About extends Component {
                         <br/> */}
                         <div className="contacts-item">
                             <a href="https://www.linkedin.com/in/ferdy-santoso-063a73149/" target="_blank" rel="noopener noreferrer">
-                                <img src={require("../../Assets/linkedin.png")} alt="linkedin" />
+                                <img src={LinkedInLogo} alt="linkedin" />
                                 <h6>Ferdy Santoso</h6>
                             </a>
                         </div>
                         <br />
                         <div className="contacts-item">
                             <a href="https://github.com/ferdysan360" target="_blank" rel="noopener noreferrer">
-                                <img src={require("../../Assets/github.png")} alt="github"/>
+                                <img src={GithubLogo} alt="github"/>
                                 <h6>ferdysan360</h6>
                             </a>
                         </div>
                         <br/>
                         <div className="contacts-item">
                             <a href="https://gitlab.com/13517116" target="_blank" rel="noopener noreferrer">
-                                <img src={require("../../Assets/gitlab.png")} alt="gitlab"/>
+                                <img src={GitlabLogo} alt="gitlab"/>
                                 <h6>ferdysan</h6>
                             </a>
                         </div>
                         <br/>
                         <div className="contacts-item">
                             <a href="mailto:ferdysantoso9999@gmail.com" target="_blank" rel="noopener noreferrer">
-                                <img src={require("../../Assets/gmail.png")} alt="gmail"/>
+                                <img src={GmailLogo} alt="gmail"/>
                                 <h6>ferdysantoso9999@gmail.com</h6>
                             </a>
                         </div>
